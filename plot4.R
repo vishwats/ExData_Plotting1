7f53fe224fec5d76aca88f2dd01520eb3741a859
vishwats <vishwats71@gmail.com>
2016-07-05 22:38:23 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,21 @@
+#Data Loading and Preprocessing
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+        temp <- tempfile()
+        download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        file <- unzip(temp)
+        unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+Set1 <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
 plot4 <- function() {
   par(mfrow=c(2,2))
   
