d451b04067885712b587ddb3207f32f6845a0929
vishwats <vishwats71@gmail.com>
2016-07-06 16:19:48 +0530

    Add files via upload

---
+++ download plot data.R
@@ -0,0 +1,6 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
